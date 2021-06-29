e8b671ed71741442f6b68dd21aa4d00221b93c2c
Em <me@lazerwalker.com>
2021-06-29 03:26:10 +0000

    Migrate from Cordova to Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lazerwalker.walk-the-world',
+  appName: 'Walk The World',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
